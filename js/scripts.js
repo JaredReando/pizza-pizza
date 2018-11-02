d45fef1ebde5aca88f28ba1cd99c96348600c523
Jared Reando <jaredreando@gmail.com>
2018-11-02 10:49:40 -0700

    Add prototype for adding pizza toppings

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,63 @@ function Pizza(toppiongs, size, id) {
   this.id = id
 }
 
+'pepperoni', 'cheese', 'onions', 'olives', ''
 
+Pizza.prototype.addOrder = function(pizza) {
+  this.toppings.push(pizza);
+}
+
+
+
+
+
+Player.prototype.rollDice = function(group) {
+  if (this.turnStatus === false) {
+    console.log("Hey! It's not your turn yet.")
+    return "";
+  }
+
+  var protoDiceResult = Math.floor(Math.random()*6)+1;
+  diceShowUI = protoDiceResult;
+
+  if (protoDiceResult === 1) {
+    this.roundScore = 0;
+    this.turnStatus = false;
+    this.diceRollResult = 0;
+    group.turnCheck(this);
+    console.log("sorry, your turn is over");
+    console.log("You rolled a " + protoDiceResult);
+    console.log("Your round score is: " + this.roundScore);
+    console.log("Your Total score is: " + this.totalScore);
+    return "";
+  } else {
+    this.diceRollResult = protoDiceResult;
+    this.roundScore += this.diceRollResult;
+    console.log("You rolled a " + protoDiceResult);
+    console.log("Your round score is: " + this.roundScore);
+    console.log("Your Total score is: " + this.totalScore);
+    return "";
+  }
+}
+
+Player.prototype.playerHold = function (group) {
+  if (this.turnStatus === false) {
+    console.log("Hey! It's not your turn yet.");
+    return "";
+  }
+  this.totalScore += this.roundScore;
+  this.roundScore = 0;
+  this.diceRollResult = 0;
+  this.turnStatus = false;
+  group.turnCheck(this);
+  if (this.totalScore >= 100) {
+    console.log("You won the game!")
+
+  } else {
+    this.turnStatus = false;
+    return "";
+  }
+}
 
 
 
