7dbe8818e4443a4567ac486cae3906dc34030ac3
Jared Reando <jaredreando@gmail.com>
2018-11-02 11:02:01 -0700

    Fix issue pushing multiple toppings into object array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // BUSINESS LOGIC --------------
-function Pizza(toppiongs, size, id) {
+function Pizza(toppings, size, id) {
   this.toppings = toppings,
   this.size = size,
   this.id = id
@@ -15,53 +15,53 @@ Pizza.prototype.addOrder = function(pizza) {
 
 
 
-Player.prototype.rollDice = function(group) {
-  if (this.turnStatus === false) {
-    console.log("Hey! It's not your turn yet.")
-    return "";
-  }
-
-  var protoDiceResult = Math.floor(Math.random()*6)+1;
-  diceShowUI = protoDiceResult;
-
-  if (protoDiceResult === 1) {
-    this.roundScore = 0;
-    this.turnStatus = false;
-    this.diceRollResult = 0;
-    group.turnCheck(this);
-    console.log("sorry, your turn is over");
-    console.log("You rolled a " + protoDiceResult);
-    console.log("Your round score is: " + this.roundScore);
-    console.log("Your Total score is: " + this.totalScore);
-    return "";
-  } else {
-    this.diceRollResult = protoDiceResult;
-    this.roundScore += this.diceRollResult;
-    console.log("You rolled a " + protoDiceResult);
-    console.log("Your round score is: " + this.roundScore);
-    console.log("Your Total score is: " + this.totalScore);
-    return "";
-  }
-}
-
-Player.prototype.playerHold = function (group) {
-  if (this.turnStatus === false) {
-    console.log("Hey! It's not your turn yet.");
-    return "";
-  }
-  this.totalScore += this.roundScore;
-  this.roundScore = 0;
-  this.diceRollResult = 0;
-  this.turnStatus = false;
-  group.turnCheck(this);
-  if (this.totalScore >= 100) {
-    console.log("You won the game!")
-
-  } else {
-    this.turnStatus = false;
-    return "";
-  }
-}
+// Player.prototype.rollDice = function(group) {
+//   if (this.turnStatus === false) {
+//     console.log("Hey! It's not your turn yet.")
+//     return "";
+//   }
+//
+//   var protoDiceResult = Math.floor(Math.random()*6)+1;
+//   diceShowUI = protoDiceResult;
+//
+//   if (protoDiceResult === 1) {
+//     this.roundScore = 0;
+//     this.turnStatus = false;
+//     this.diceRollResult = 0;
+//     group.turnCheck(this);
+//     console.log("sorry, your turn is over");
+//     console.log("You rolled a " + protoDiceResult);
+//     console.log("Your round score is: " + this.roundScore);
+//     console.log("Your Total score is: " + this.totalScore);
+//     return "";
+//   } else {
+//     this.diceRollResult = protoDiceResult;
+//     this.roundScore += this.diceRollResult;
+//     console.log("You rolled a " + protoDiceResult);
+//     console.log("Your round score is: " + this.roundScore);
+//     console.log("Your Total score is: " + this.totalScore);
+//     return "";
+//   }
+// }
+//
+// Player.prototype.playerHold = function (group) {
+//   if (this.turnStatus === false) {
+//     console.log("Hey! It's not your turn yet.");
+//     return "";
+//   }
+//   this.totalScore += this.roundScore;
+//   this.roundScore = 0;
+//   this.diceRollResult = 0;
+//   this.turnStatus = false;
+//   group.turnCheck(this);
+//   if (this.totalScore >= 100) {
+//     console.log("You won the game!")
+//
+//   } else {
+//     this.turnStatus = false;
+//     return "";
+//   }
+// }
 
 
 
